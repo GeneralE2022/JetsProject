892cbfa56e8249451fae5883f73b3ded1bf3cbde
Erich Schindler <Erich@Schindler-iMac.local>
2022-04-01 17:53:39 -0400

    initial commit

---
+++ src/Readme.md
@@ -0,0 +1,26 @@
+## Jets Project 
+
+### Week 3 Homework for Skill Distillery 
+
+## Overview 
+
+Jets fleet management 
+
+[insert user stories] 
+
+
+
+## Technologies / Skills used 
+* Java 
+* Eclipse 
+* Arrays / ArrayList 
+* Encapsulation 
+* Polymorphism 
+* Abstract classes 
+* Setters / getters 
+* Constructors 
+* the toString method 
+
+## Lessons learned 
+* [complete] 
+* Shout out to the brilliant SD TAs! 
---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,34 @@
+package com.skilldistillery.jets.app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+
+//public class JetsApplication {
+//
+//	public static void main(String[] args) {
+//		
+//		JetsApplication jetApp = new JetsApplication(); 
+//		
+//		
+//		jetApp.readFromFile("jets.txt", ArrayList<String> jetsList);
+//	
+//	
+//	}
+//	
+//	
+//	public void readFromFile(String fn, List<String> jetsList) {
+//		try {
+//			BufferedReader bufIn = new BufferedReader( new FileReader(fn) );
+//			String line;
+//			
+//			while ( (line = bufIn.readLine()) != null ) {
+//				namesColl.add(line);
+//			}
+//			
+//		}  catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//}
